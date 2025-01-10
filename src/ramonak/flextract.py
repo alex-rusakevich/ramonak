655fcfc486c62d83a48eaa8867ab31985b7936a8
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-01-10 12:33:46 +0300

    #patch synthesized valuable flexions

--- src/ramonak/flextract.py
+++ src/ramonak/flextract.py
@@ -1,7 +1,16 @@
 from os.path import commonprefix
-from typing import List
+from typing import List, Tuple
+from pathlib import Path
+from collections import Counter
+import gc
+from ramonak.packages.actions import package_path as rm_pkg_path
 
 from lxml import etree
+
+from ramonak.packages.actions import install as rm_install
+
+
+rm_install("@bnkorpus/grammar_db/20230920")
 
 
 def find_flexions(words) -> List[str]:
@@ -32,31 +41,50 @@ def extract_flexions(ncorp_xml_path) -> List[str]:
     flexions_in_file = []
 
     for variant in root.findall("Paradigm/Variant"):
+        variant_lemma = variant.get("lemma").replace("+", "")
         forms = []
 
+        if "-" in variant_lemma:
+            continue
+
         for form in variant.findall("Form"):
-            form_content = form.text.replace("+", "")
-
-            if "-" in form_content:
-                continue
-
             forms.append(form.text.replace("+", ""))
 
-        flexions = find_flexions(forms)
+        flexions = find_flexions((variant_lemma, *forms))
         flexions_in_file.extend(flexions)
 
-    print("Flexions were found. Counting...")
+    return flexions_in_file
 
-    # flexions_in_file = filter(lambda x: len(x) <= 3, flexions_in_file)
-    flexions_count = []
 
-    for flexion in set(flexions_in_file):
-        flexions_count.append(
-                (flexion, flexions_in_file.count(flexion))
+def xml_flexion_stats(xml_dir_path: str) -> Tuple[Tuple[str, int]]:
+    all_flexions = []
+
+    for xml_file in Path(xml_dir_path).glob("*.xml"):
+        print("Processing", xml_file)
+        all_flexions.extend(
+                extract_flexions(xml_file)
                 )
+        gc.collect()
 
-    flexions_count = sorted(flexions_count, key=lambda x: x[1], reverse=True)
+    print("All flexions: {}. Counting total stats...".format(len(all_flexions)))
 
-    print(flexions_count)
+    flexions_and_count = Counter(all_flexions).items()
 
-    return set(flexions_in_file)
+    print("Flexions: {}. Sorting...".format(len(flexions_and_count)))
+
+    flexions_and_count = sorted(flexions_and_count, key=lambda x: x[1], reverse=True)
+
+    return flexions_and_count
+
+
+def get_flexions():
+    flexions_and_count = xml_flexion_stats(rm_pkg_path("@bnkorpus/grammar_db/20230920"))
+    max_count = flexions_and_count[0][1]
+
+    flexions_and_count = map(lambda x: (x[0], round(x[1]/max_count, 1)), flexions_and_count)
+    flexions_and_count = tuple(filter(lambda x: x[1] > 0, flexions_and_count))
+
+    print("Valuable flexions: {}".format(len(flexions_and_count)))
+
+    return tuple(x[0] for x in flexions_and_count)
+
--- src/ramonak/interactive.py
+++ src/ramonak/interactive.py
@@ -1,11 +1,6 @@
-from pathlib import Path
+from ramonak.flextract import get_flexions
 
-from ramonak.flextract import extract_flexions
-from ramonak.packages.actions import install as rm_install, package_path as rm_pkg_path
+flexions = get_flexions()
 
-rm_install("@bnkorpus/grammar_db/20230920")
-
-flexions = extract_flexions(Path(rm_pkg_path("@bnkorpus/grammar_db/20230920"), "A1.xml"))
-
-print("Знойдзена", len(flexions), "суфіксаў")
+print("Знойдзена", len(flexions), "флекс.")
 print(flexions)
