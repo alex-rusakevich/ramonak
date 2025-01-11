da0b69c778a437bf116faa593de2807387ccc2cc
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-01-11 21:22:49 +0300

    #patch added stem_words feature

--- src/ramonak/stemmer/base.py
+++ src/ramonak/stemmer/base.py
@@ -25,3 +25,11 @@ class Stemmer:
         # endregion
 
         return word
+
+    def stem_words(self, words: list[str]) -> list[str]:
+        result = []
+
+        for word in words:
+            result.append(self.stem_word(word))
+
+        return result
